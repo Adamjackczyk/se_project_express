2ed65c87ed63504049223d5a97c57dd6dbff9b09
Jack Adamczyk <prepinavest@gmail.com>
2024-09-22 09:35:09 -0400

    add schemas and validators

---
+++ models/clothingItem.js
@@ -0,0 +1,40 @@
+const clothingItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Item name is required"],
+    minlength: 2,
+    maxlength: 30,
+  },
+  weather: {
+    type: String,
+    required: [true, "Weather type is required"],
+    enum: ["hot", "warm", "cold"],
+    default: "hot",
+  },
+  imageUrl: {
+    type: String,
+    required: [true, "Image URL is required"],
+    validate: {
+      validator: function (value) {
+        return validator.isURL(value);
+      },
+      message: "You must enter a valid URL",
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  likes: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: "user",
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("clothingItem", clothingItemSchema);
